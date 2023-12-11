a8052ead25c1fb35f16197edce75ed89477843a9
Lele1818 <148886814+Lele1818@users.noreply.github.com>
2023-12-11 18:08:15 +0700

    Update add_to_order.php

--- view/add_to_order.php
+++ view/add_to_order.php
@@ -1,23 +1,45 @@
 <?php
+require_once('db_connection.php');
 
-require_once('db_connnection.php');
-$conn = OpenCon();
+// Kiểm tra xem có dữ liệu được gửi từ trang sản phẩm không
+if (isset($_POST['productID']) && isset($_POST['userNote']) && isset($_POST['selectedOrder'])) {
+    $productID = $_POST['productID'];
+    $userNote = $_POST['userNote'];
+    $selectedOrder = $_POST['selectedOrder'];
 
-$productID = $_POST['productID'];
-$Name = $_POST['productName'];
-$price = $_POST['price'];
-$userNote = $_POST['userNote'];
-
-$query = "INSERT INTO Order (Product_ID, Name, Price, Note) VALUES ('$productID', '$Name', '$price', '$userNote')";
-$result = $conn->query($query);
-
-// Kiểm tra và thông báo kết quả
-if ($result) {
-    echo "Sản phẩm đã được thêm vào đơn hàng thành công!";
+    // Gọi hàm thêm sản phẩm vào đơn hàng
+    addToOrder($productID, $userNote, $selectedOrder);
 } else {
-    echo "Có lỗi khi thêm sản phẩm vào đơn hàng: " . $conn->error;
+    // Nếu không có dữ liệu, trả về mã lỗi
+    http_response_code(400);
+    echo 'Invalid data';
 }
 
-// Đóng kết nối
-CloseCon($conn);
+function addToOrder($productID, $userNote, $selectedOrder) {
+    $conn = OpenCon();
+
+    // Thực hiện thêm sản phẩm vào đơn hàng
+    $query = "INSERT INTO Order_Line (Order_ID, Item_ID, Quantity, Total_Price)
+              VALUES (?, ?, 1, (SELECT Price FROM Product WHERE Product_ID = ?))";
+
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("sss", $selectedOrder, $productID, $productID);
+
+    if ($stmt->execute()) {
+        // Thêm ghi chú vào đơn hàng (Order)
+        $updateOrderQuery = "UPDATE `Order` SET Note = ? WHERE Order_ID = ?";
+        $updateOrderStmt = $conn->prepare($updateOrderQuery);
+        $updateOrderStmt->bind_param("ss", $userNote, $selectedOrder);
+        
+        if ($updateOrderStmt->execute()) {
+            echo 'Product added to order successfully';
+        } else {
+            echo 'Failed to update order note';
+        }
+    } else {
+        echo 'Failed to add product to order';
+    }
+
+    CloseCon($conn);
+}
 ?>
