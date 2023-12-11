b3f0ba382130aa5aa92f657f509013666cd1f0d5
Lele1818 <148886814+Lele1818@users.noreply.github.com>
2023-12-12 04:19:31 +0700

    Update index.php

--- view/view_detail/index.php
+++ view/view_detail/index.php
@@ -1,52 +1,48 @@
-<!doctype html>
-<html lang='en'>
+<?php
+require_once('db_connnection.php');
+if (isset($_GET['Product_ID'])) {
+    $conn = OpenCon();
+    $maMonAn = $_GET['Product_ID'];
+    
 
-<head>
-    <meta charset='utf-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>Trang chi tiết món ăn</title>
-    <link href='cover.css' rel='stylesheet'>
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65' crossorigin='anonymous'>
-</head>
+    // Use prepared statements to prevent SQL injection
+    $query = "SELECT * FROM `Product` WHERE Product_ID = ?;";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $maMonAn);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-<body class=''>
-    <main class='main-content w-100 m-auto'>
-        
-        <?php
-        if (isset($_GET['Produce_ID'])) {
-            require_once('db_connnection.php');
+    if ($result->num_rows > 0) {
+        // OUTPUT DATA OF EACH ROW
+        while ($row = $result->fetch_assoc()) {
+            $maMonAn = $row['Product_ID'];
+            $tenMonAn = $row['Name'];
+            $moTaMonan = $row['Description'];
+            $giaNiemYet = $row['Price'];
+            $size = $row['Size'];
+            $category = $row['Category'];
 
-            $conn = OpenCon();
-            $maMonAn = $_GET['Produce_ID'];
-            $query = "SELECT * FROM Produce WHERE Product_ID = '$maMonAn';";
+            // Fetching the image path directly from the row
+            $result2 = $row['Photo'];
 
-            $result = $conn->query($query);
-
-            if ($result->num_rows > 0) {
-                // OUTPUT DATA OF EACH ROW
-                while ($row = $result->fetch_assoc()) {
-                    $maMonAn = $row['Product_ID'];
-                    $tenMonAn = $row['Name'];
-                    $moTaMonan = $row['Description'];
-                    $giaNiemYet = $row['Price'];
-                    $query = "SELECT * FROM `Product` WHERE Product_ID = '$maMonAn';";
-
-                    $result1 = $conn->query($query);
-                    $result2 = $result1->fetch_assoc()['Photo'];
-
-                    echo "<div class='card'>
+            echo "<div class='card'>
                 <img src='$result2' class='card-img-top' alt=''>
                 <div class='card-body'>
                     <h5 class='card-title'>$tenMonAn</h5>
                     <p class='card-text'>$moTaMonan</p>
+                    <p class='card-text'>Price: $giaNiemYet</p>
+                    <p class='card-text'>Size: $size</p>
+                    <p class='card-text'>Category: $category</p>
                     <a href='../' class='btn btn-primary'>Trở về trang danh sách sản phẩm</a>
                 </div>
             </div>";
-                }
-            }
         }
-        ?>
-    </main>
-</body>
+    } else {
+        echo "No data found for the specified Product_ID.";
+    }
 
-</html>
+    // Close the prepared statement and database connection
+    $stmt->close();
+    CloseCon($conn);
+}
+?>
